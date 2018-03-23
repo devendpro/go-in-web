ddf3ec9f0a878588c200ad0068c1c7557ad79f67
William Martins <devendpro@gmail.com>
2018-03-23 20:00:24 -0300

    Diretorios 22 e 23 finalizados

---
+++ 022/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Panic(err)
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+	}
+
+	defer conn.Close()
+
+	// Não usar o GET aqui
+	// Temos aqui uma conexão de stream aberta
+	// Como saber quando o leitor terminou?
+	fmt.Println("Codificado até aqui...")
+
+}
